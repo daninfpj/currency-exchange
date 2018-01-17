06b0ef6c39851960d1c16aa48f5acced174092ca
Daniel Infante Vargas <daninfpj@gmail.com>
2018-01-16 19:54:11 -0500

    Add support for multiple default currencies to convert to.

--- convert.py
+++ convert.py
@@ -10,81 +10,108 @@ log = None
 def main(wf):
     # Get args from Workflow as normalized Unicode
     args = wf.args
-    
-    if not 'defaults' in wf.settings:
-        wf.settings['defaults'] = { 'fr': 'USD', 'to': 'EUR' }
-    
+
+    check_settings(wf)
+
     if len(args):
         if args[0] == 'from' or args[0] == 'to':
             return set_defaults(args)
-        
+
     fr = wf.settings['defaults']['fr']
     to = wf.settings['defaults']['to']
     amount = 1
-    
+
     if len(args):
         if args[0].replace('.', '', 1).isdigit():
             amount = float(args[0])
             num_args = 2
         else:
             num_args = 1
-        
-        if len(args) == num_args:
-          fr = args[num_args - 1].upper()
-        elif len(args) == num_args + 1:
-          fr = args[num_args - 1].upper()
-          to = args[num_args].upper()
-    
-        if len(fr) != 3 or len(to) != 3:
+
+        if len(args) >= num_args:
+            fr = validate_cur(args[num_args - 1])
+        if len(args) == num_args + 1:
+            to = [validate_cur(args[num_args])]
+
+        if not fr or not to[0]:
             wait()
-    
-    rate = get_rate(fr, to)
-    
-    if not rate:
-        rate = get_rate_alt(fr, to)
-        
-    value = amount * rate
+            return
 
-    formatted = "{:.2f}".format(value)
-    
-    # Add an item to Alfred feedback
-    wf.add_item('%d %s = %s %s' % (amount, fr, formatted, to), valid=True, subtitle='Press enter to copy to clipboard', copytext=formatted, arg=formatted)
+    rates = []
 
-    # Send output to Alfred
-    wf.send_feedback()
-    
-def set_defaults(args):
-    if len(args[1]) == 3:
-        cur = args[1].upper()
-        
-        if args[0] == 'from':
-            wf.settings['defaults'] = { 'fr': cur, 'to': wf.settings['defaults']['to'] }
-        if args[0] == 'to':
-            wf.settings['defaults'] = { 'fr': wf.settings['defaults']['fr'], 'to': cur }
+    for cur in to:
+        rate = get_rate(fr, cur)
 
-        print cur
+        if rate:
+            rates.append(rate)
+
+            value = amount * rate
+
+            formatted = "{:.2f}".format(value)
+
+            wf.add_item('%d %s = %s %s' % (amount, fr, formatted, cur), valid=True, subtitle='Press enter to copy to clipboard', copytext=formatted, arg=formatted)
+
+    if len(rates) == 0:
+        wait()
         return
-    
+    else:
+        wf.send_feedback()
+
+def check_settings(wf):
+    if not 'defaults' in wf.settings:
+        wf.settings['defaults'] = { 'fr': 'USD', 'to': ['EUR'] }
+
+    if isinstance(wf.settings['defaults']['to'], basestring):
+        wf.settings['defaults']['to'] = [wf.settings['defaults']['to']]
+
+def set_defaults(args):
+    cur = validate_cur(args[1])
+
+    if args[0] == 'from' and cur:
+        wf.settings['defaults'] = { 'fr': cur, 'to': wf.settings['defaults']['to'] }
+    if args[0] == 'to':
+        to = []
+
+        for item in args[1:]:
+            cur = validate_cur(item)
+            if cur:
+                to.append(cur)
+
+        cur = (', ').join(to)
+
+        wf.settings['defaults'] = { 'fr': wf.settings['defaults']['fr'], 'to': to }
+
+    print cur
+
+    return
+
 def wait():
     wf.add_item('Please enter a valid format', 'cur [amount] [currency code] [currency code]')
-    
+
     wf.send_feedback()
 
 def get_rate(fr, to):
     data = web.get('http://apilayer.net/api/live?access_key=c938bcf13e0aff152614745f41414ed4&currencies=%s,%s' % (fr, to)).json()
-    
+
     if data['success']:
         return (1 / data['quotes']['USD%s' % fr]) * data['quotes']['USD%s' % to]
     else:
-        return None
+        return get_rate_alt(fr, to)
+
+    return res
 
 def get_rate_alt(fr, to):
-    data = web.get('http://free.currencyconverterapi.com/api/v3/convert?q=%s_%s&compact=ultra' % (fr, to)).json()
-    
     try:
+        data = web.get('http://free.currencyconverterapi.com/api/v3/convert?q=%s_%s&compact=ultra' % (fr, to)).json()
         return data['%s_%s' % (fr, to)]
     except:
-        wait()
+        return None
+
+def validate_cur(cur):
+    if len(cur) == 3:
+        return cur.upper()
+    else:
+        return None
 
 if __name__ == '__main__':
     wf = Workflow()
